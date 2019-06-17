13f6996517cf2bb75befbaadcf2b2511b8c19e9d
Olawale <adedekojoseph@gmail.com>
2019-06-17 21:58:57 +0100

    add update car functionality

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -2,6 +2,7 @@ import Cars from '../controllers/cars';
 import express from 'express';
 import imageMiddleware from '../middlewares/imageParser';
 import verifyToken from '../middlewares/veirfyToken';
+import verifyCarUpdate from '../middlewares/updateCar';
 
 const router = express.Router()
 
@@ -11,5 +12,7 @@ router.get('/', Cars.getAllCars);
 router.get('/:id', Cars.getOneCar)
 
 router.post("/",verifyToken, imageMiddleware, Cars.add);
+
+router.patch('/:id', verifyToken,verifyCarUpdate, Cars.updateCar)
 
 export default router;
